628a770bf64b4a688ef14cfd37499ed07c6d03bd
ugsdw-admin <ugsdw.org@gmail.com>
2024-07-31 15:49:07 -0500

    stuff

--- _plugins/liquidfilter.rb
+++ _plugins/liquidfilter.rb
@@ -1,13 +1,12 @@
 # For using liquid in Jekyll frontmatter. Source: https://github.com/gemfarmer/jekyll-liquify
 
-module Jekyll
-  module LiquifyFilter
-    def liquify(input)
-      if input.is_a? String
-        Liquid::Template.parse(input).render(@context)
-      else
-        input
-      end
+
+module LiquifyFilter
+  def liquify(input)
+    if input.is_a? String
+      Liquid::Template.parse(input).render(@context)
+    else
+      input
     end
   end
 end
